41c9b0c6032fb9bf760d85dfec1a163f3856f73b
dushyantsolanki <dushyantsolanki.2002@gmail.com>
2022-12-18 17:55:28 +0530

    Mobile Responsive Desgine Bug Fixed

--- script.js
+++ script.js
@@ -5,9 +5,6 @@ let a = document.body.getElementsByTagName("input")[0];
 // addEventListner In Button
 
 a.addEventListener("click", (e) => {
-  document
-    .getElementsByClassName("back-color")[0]
-    .classList.toggle("dark-back-color");
 
   document.getElementsByClassName("it")[0].classList.toggle("dark-it");
   document.getElementsByClassName("it")[1].classList.toggle("dark-it");
@@ -34,24 +31,23 @@ a.addEventListener("click", (e) => {
   w[2].classList.toggle("dark-h2");
   w[3].classList.toggle("dark-h2");
 
-//     bottom cards 
+  //     bottom cards
 
-let q = document.getElementsByTagName("h3");
+  let q = document.getElementsByTagName("h3");
 
-q[0].classList.toggle("dark-h3");
-q[1].classList.toggle("dark-h3");
-q[2].classList.toggle("dark-h3");
-q[3].classList.toggle("dark-h3");
-q[4].classList.toggle("dark-h3");
-q[5].classList.toggle("dark-h3");
-q[6].classList.toggle("dark-h3");
-q[7].classList.toggle("dark-h3");
-q[8].classList.toggle("dark-h3");
+  q[0].classList.toggle("dark-h3");
+  q[1].classList.toggle("dark-h3");
+  q[2].classList.toggle("dark-h3");
+  q[3].classList.toggle("dark-h3");
+  q[4].classList.toggle("dark-h3");
+  q[5].classList.toggle("dark-h3");
+  q[6].classList.toggle("dark-h3");
+  q[7].classList.toggle("dark-h3");
+  q[8].classList.toggle("dark-h3");
 
-//  For Heading 
+  //  For Heading
 
-document.getElementsByTagName("h1")[0].classList.toggle("dark-h1")
+  document.getElementsByTagName("h1")[0].classList.toggle("dark-h1");
 
-document.getElementsByTagName("h5")[0].classList.toggle("dark-h5");
-
+  document.getElementsByTagName("h5")[0].classList.toggle("dark-h5");
 });
